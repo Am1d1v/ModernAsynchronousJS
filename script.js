36c10a6ef3563deae9ed008ade60946552bf2db0
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-27 13:24:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ async function getProducts(){
     try {
 
     const productsResponse = await fetch('https://dummyjson.com/products');
+        if(!productsResponse.ok) {
+            throw new Error(productsResponse.status);
+        }
+
     const {products} = await productsResponse.json();
     console.log(products);
 
-    throw new Error('Error');
+
+    //throw new Error('Error');
 
     const productResponse = await fetch('https://dummyjson.com/products/' + products[0].id);
     const product = await productResponse.json();
@@ -17,8 +22,12 @@ async function getProducts(){
 
     } catch(e){
             console.error(e);
+    } finally {
+        console.log('Finally');
     }
     
+
+   
     
 }
 
