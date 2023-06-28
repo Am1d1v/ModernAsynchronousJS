5bdadd24777e1e8f86429a6a755bf5665d5e4c41
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-28 20:36:51 +0300

    Add files via upload

---
+++ Queue.js
@@ -0,0 +1,18 @@
+
+
+const asyncArrow = async () => {
+
+    const Response = await fetch('https://dummyjson.com/products');
+    
+    const data = await Response.json();
+    return data;
+    
+}
+
+console.log('1')
+asyncArrow().then(data => { 
+    console.log(data)
+
+    
+});
+console.log('2');
